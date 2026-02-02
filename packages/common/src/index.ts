8b595b080da728df71e4852f184300e51ace346e
Raj Tejaswee <rajtejaswee02@gmail.com>
2026-02-02 05:48:03 +0530

    feat: added zod schemas

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,1 +1,21 @@
-export const a = 1;
+import {z} from "zod";
+
+export const SignupSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+    name: z.string().min(3)
+})
+
+export const SigninSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6)
+})
+
+export const CreateRoomSchema = z.object({
+    slug: z.string().min(3).max(20),
+})
+
+export type SignupType = z.infer<typeof SignupSchema>
+export type SigninType = z.infer<typeof SigninSchema>
+export type CreateRoomType = z.infer<typeof CreateRoomSchema>
+
