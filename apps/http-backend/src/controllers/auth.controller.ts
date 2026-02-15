f2cdfc0b13243a33f0bf65646d1220f3712c9562
Raj Tejaswee <rajtejaswee02@gmail.com>
2026-02-15 20:34:02 +0530

    nuclear fix v3

--- apps/http-backend/src/controllers/auth.controller.ts
+++ apps/http-backend/src/controllers/auth.controller.ts
@@ -1,4 +1,4 @@
-// Fixing import error v2
+// NUCLEAR FIX V3
 import { Request, Response } from "express";
 import {SignupSchema, SigninSchema} from "@repo/common";
 import { prismaClient } from "@repo/db";
