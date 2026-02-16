d33444eec6c1b54a91de8f9c5d36e6545c922aeb
Raj Tejaswee <rajtejaswee02@gmail.com>
2026-02-17 02:48:14 +0530

    fix: fixed backend errors

--- apps/http-backend/src/app.ts
+++ apps/http-backend/src/app.ts
@@ -1,9 +1,9 @@
 import "dotenv/config";
-import express from "express";
+import express, { Application } from "express";
 import cors from "cors";
 import { prismaClient } from "@repo/db";
 
-const app = express();
+const app: Application = express();
 
 
 const allowedOrigins = [
